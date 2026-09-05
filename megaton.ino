f496508116663d88760d97fab7a301b0752e3531
gabrielle carvalho <gabiecarvalho15@gmail.com>
2026-09-05 08:29:56 -0300

    chore: renomeando codigo autonomo e voltando versao do megation que esta na dev

--- megaton.ino
+++ megaton.ino
@@ -1,10 +1,13 @@
+
 unsigned long tempoUltimaLeitura = 0;
-const unsigned long intervaloLeitura = 50; // 200 é como tava antes
-long distanciaAtual = 999; // valor padrão distante
+const unsigned long intervaloLeitura = 200; // 500 ms
+long distanciaAtual = 0;
+
 
 //Sensor ultrassônico:
-const int pin_echo = 15; // INPUT
-const int pin_trig = 2; // OUTPUT
+const int pin_echo = 15;
+const int pin_trig = 2;
+
 
 // Sensores infravermelhos:
 const int sensor_direita = 23;
@@ -13,6 +16,7 @@ const int sensor_esquerda = 13;
 // Valores PwM utilizada no motor:
 const int velocidade = 255;
 const int parado = 0;
+const int semivelocidade = 100;
 
 // Motores que vão para frente:
 const int motorRightFront_Go = 22;
@@ -26,21 +30,20 @@ const int motorLeftFront_Back = 16;
 const int motorRightBack_Back = 19;
 const int motorLeftBack_Back = 17;
 
-
 void setup() {
 // Criação dos canais em seus respectivos pinos:
-    // Configuração dos canais PWM
-    pinMode(motorRightFront_Go, OUTPUT);
-    pinMode(motorLeftFront_Go, OUTPUT);
-    pinMode(motorRightBack_Go, OUTPUT);
-    pinMode(motorLeftBack_Go, OUTPUT);
-
-    pinMode(motorRightFront_Back, OUTPUT);
-    pinMode(motorLeftFront_Back, OUTPUT);
-    pinMode(motorRightBack_Back, OUTPUT);
-    pinMode(motorLeftBack_Back, OUTPUT);
-
-
+    for (int i = 0; i < 8; i++)
+    {
+        ledcSetup(i, 5000, 8);
+    }
+    ledcAttachPin(motorRightFront_Go, 0);
+    ledcAttachPin(motorLeftFront_Go, 1);
+    ledcAttachPin(motorRightBack_Go, 2);
+    ledcAttachPin(motorLeftBack_Go, 3);
+    ledcAttachPin(motorRightFront_Back, 4);
+    ledcAttachPin(motorLeftFront_Back, 5);
+    ledcAttachPin(motorRightBack_Back, 6);
+    ledcAttachPin(motorLeftBack_Back, 7);
     // Sensores:
     Serial.begin(9600);
     pinMode(sensor_direita, INPUT);
@@ -49,118 +52,76 @@ void setup() {
     pinMode(pin_trig, OUTPUT);
 } 
 
-long distancia_inimigo()
-{
+int distancia_inimigo() {
     unsigned long agora = millis();
-    if (agora - tempoUltimaLeitura >= intervaloLeitura){
+    if (agora - tempoUltimaLeitura >= intervaloLeitura)
         tempoUltimaLeitura = agora;
         digitalWrite(pin_trig, LOW);
         delayMicroseconds(2);
         digitalWrite(pin_trig, HIGH);
         delayMicroseconds(10);
         digitalWrite(pin_trig, LOW);
-        
         long duracao = pulseIn(pin_echo, HIGH, 30000); // timeout de 30ms
-        
-        if (duracao == 0) {
-            distanciaAtual = 999; // sem alvo detectado
-        } else {
-            distanciaAtual = duracao * 0.034 / 2;
-        }
-    }
-    return distanciaAtual; // retorna o último valor lido válido
+        distanciaAtual = duracao * 0.034 / 2;
+        return distanciaAtual;
 }
-
+ 
 void ir_frente() {
-    analogWrite(motorRightFront_Go, velocidade);
-    analogWrite(motorLeftFront_Go, velocidade);
-    analogWrite(motorRightBack_Go, velocidade);
-    analogWrite(motorLeftBack_Go, velocidade);
-
-    analogWrite(motorRightFront_Back, parado);
-    analogWrite(motorLeftFront_Back, parado);
-    analogWrite(motorRightBack_Back, parado);
-    analogWrite(motorLeftBack_Back, parado);
+    ledcWrite(0, velocidade);
+    ledcWrite(1, velocidade);
+    ledcWrite(2, velocidade);
+    ledcWrite(3, velocidade);
+    ledcWrite(4, parado);
+    ledcWrite(5, parado);
+    ledcWrite(6, parado);
+    ledcWrite(7, parado);
 }
 
 void ir_tras() {
-    analogWrite(motorRightFront_Go, parado);
-    analogWrite(motorLeftFront_Go, parado);
-    analogWrite(motorRightBack_Go, parado);
-    analogWrite(motorLeftBack_Go, parado);
-
-    analogWrite(motorRightFront_Back, velocidade);
-    analogWrite(motorLeftFront_Back, velocidade);
-    analogWrite(motorRightBack_Back, velocidade);
-    analogWrite(motorLeftBack_Back, velocidade);
+    ledcWrite(0, parado);
+    ledcWrite(1, parado);
+    ledcWrite(2, parado);
+    ledcWrite(3, parado);
+    ledcWrite(4, velocidade);
+    ledcWrite(5, velocidade);
+    ledcWrite(6, velocidade);
+    ledcWrite(7, velocidade);
 }
 
 void girar_direita() {
-    analogWrite(motorRightFront_Go, parado);
-    analogWrite(motorLeftFront_Go, velocidade);
-    analogWrite(motorRightBack_Go, parado);
-    analogWrite(motorLeftBack_Go, velocidade);
-
-    analogWrite(motorRightFront_Back, velocidade);
-    analogWrite(motorLeftFront_Back, parado);
-    analogWrite(motorRightBack_Back, velocidade);
-    analogWrite(motorLeftBack_Back, parado);
+    ledcWrite(0, parado);
+    ledcWrite(1, velocidade);
+    ledcWrite(2, parado);
+    ledcWrite(3, velocidade);
+    ledcWrite(4, velocidade);
+    ledcWrite(5, parado);
+    ledcWrite(6, velocidade);
+    ledcWrite(7, parado);
 }
 
 void girar_esquerda() {
-    analogWrite(motorRightFront_Go, velocidade);
-    analogWrite(motorLeftFront_Go, parado);
-    analogWrite(motorRightBack_Go, velocidade);
-    analogWrite(motorLeftBack_Go, parado);
-
-    analogWrite(motorRightFront_Back, parado);
-    analogWrite(motorLeftFront_Back, velocidade);
-    analogWrite(motorRightBack_Back, parado);
-    analogWrite(motorLeftBack_Back, velocidade);
+    ledcWrite(0, velocidade);
+    ledcWrite(1, parado);
+    ledcWrite(2, velocidade);
+    ledcWrite(3, parado);
+    ledcWrite(4, parado);
+    ledcWrite(5, velocidade);
+    ledcWrite(6, parado);
+    ledcWrite(7, velocidade);
 }
 
-void loop() {
+void loop()
+{
     int direita = digitalRead(sensor_direita);
     int esquerda = digitalRead(sensor_esquerda);
+    long tempo = 0;
     long distancia = distancia_inimigo();
-
-    if (esquerda == 0 && direita == 0) {
-        ir_tras();
-        delay(400);
-    } 
-
-    else if (esquerda == 0) {  // detectou a borda pela esquerda e vai avançar pela direira pra o adversario cair
-        girar_direita();
-        delay(150);
-        
+    girar_direita();
+    if (distancia <= 60)
+    {
         ir_frente();
-        delay(250);
-        
-        // Verifica se ainda está no limite após o ataque antes de recuar
-        if (digitalRead(sensor_esquerda) == 0 || digitalRead(sensor_direita) == 0) { //recua pq chegou na borda
+        if (esquerda == 0 || direita == 0){
             ir_tras();
-            delay(300);
-        }
-    } 
-
-    else if (direita == 0) {  // detectou a borda pela direita e vai avançar pela esquerda pra o adversario cair
-        girar_esquerda();
-        delay(150);
-        
-        ir_frente();
-        delay(250);
-        
-        if (digitalRead(sensor_esquerda) == 0 || digitalRead(sensor_direita) == 0) { //recua pq chegou na borda
-            ir_tras();
-            delay(300);
-        }
-    } 
-    
-    else { // busca e ataque normal
-        if (distancia > 0 && distancia <= 60) {
-            ir_frente(); // ataca
-        } else {
-            girar_direita(); // procura
         }
     }
 }
